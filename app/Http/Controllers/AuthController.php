9fe2d4844b79dd767bd0ca5adea467ff261ce0e6
Okan Aslan <aaslan.okan@gmail.com>
2024-08-20 14:10:02 +0300

    created controller & added authentication logics

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ApiResponses;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function register(RegisterUserRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $user = User::create($validated);
+
+        return $this->success($user, "User created successfully.", 201);
+    }
+
+    public function login(LoginUserRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return $this->error(null, "The provided credentials are incorrect.", 500);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return $this->success([
+            'user' => $user,
+            'token' => $token,
+        ], "User logged-in successfully.", 200);
+    }
+
+    public function profile(Request $request): JsonResponse
+    {
+        return $this->success($request->user(), "User profile retrieved successfully", 200);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->tokens()->delete();
+
+        return $this->success(null, "User logged-out successfully.", 200);
+    }
+
+    public function destroy(Request $request): JsonResponse
+    {
+        $user = $request->user();
+        $user->tokens()->delete();
+        $user->delete();
+
+        return $this->success(null, "User account deleted successfully", 200);
+    }
+}
