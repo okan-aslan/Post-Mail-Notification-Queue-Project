e831dbd882a1f1d74b0317d9bed073d62e2ddc43
Okan Aslan <aaslan.okan@gmail.com>
2024-08-21 17:14:10 +0300

    added Post routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,24 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Middleware\RedirectIfAdminIndex;
+use App\Http\Middleware\RedirectIfAdminShow;
 use Illuminate\Support\Facades\Route;
 
+Route::post('register', [AuthController::class, 'register'])->name('user.register');
+Route::post('login', [AuthController::class, 'login'])->name('user.login');
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('profile', [AuthController::class, 'profile']);
-    Route::post('profile/logout', [AuthController::class, 'logout']);
-    Route::post('profile/delete', [AuthController::class, 'destroy']);
+    Route::get('profile', [AuthController::class, 'profile'])->name('user.profile');
+    Route::post('profile/logout', [AuthController::class, 'logout'])->name('user.logout');
+    Route::post('profile/delete', [AuthController::class, 'destroy'])->name('user.destory');
+
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 });
+
