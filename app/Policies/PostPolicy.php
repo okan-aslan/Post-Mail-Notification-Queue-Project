635fa937d26142d1106469b04b57c5848456c703
Okan Aslan <aaslan.okan@gmail.com>
2024-08-21 17:16:25 +0300

    created policy & added logic

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+    /**
+     * Determine whether the user can update or delete the model.
+     */
+    public function authorize(User $user, Post $post): Response
+    {
+        return $user->id === $post->user_id || $user->isAdmin()
+            ? Response::allow()
+            : Response::deny("This post doesn't belongs to you.");
+    }
+}
