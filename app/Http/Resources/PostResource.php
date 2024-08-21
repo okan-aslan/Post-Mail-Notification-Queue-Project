f28d68955e56499f579d8755a0b93e1971d8f618
Okan Aslan <aaslan.okan@gmail.com>
2024-08-21 17:15:39 +0300

    created resource & added logic

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'content' => $this->content,
+            'createdAt' => $this->created_at->format('Y-m-d H:i:s'),
+            'updatedAt' => $this->updated_at->format('Y-m-d H:i:s'),
+            'status' => $this->when(
+                $request->user() && $request->user()->isAdmin(),
+                $this->status
+            ),
+            'user' => new UserResource($this->whenLoaded('user')),
+        ];
+    }
+}
