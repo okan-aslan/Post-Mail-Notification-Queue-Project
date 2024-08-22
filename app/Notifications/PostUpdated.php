2d93e29a43e64b7923b6053425a54dd40d1578d7
Okan Aslan <aaslan.okan@gmail.com>
2024-08-22 14:15:08 +0300

    created new notification for post update

---
+++ app/Notifications/PostUpdated.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PostUpdated extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $post;
+
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+        $this->onConnection('redis');
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('A Post Updated: ' . $this->post->title)
+            ->greeting('Hello, Admin!')
+            ->line('A post has been updated.')
+            ->line('Title: ' . $this->post->title)
+            ->line('Content: ' . $this->post->content)
+            ->action('Publish post', url("/posts/{$this->post->id}/publish"))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'post_id' => $this->post->id,
+            'post_title' => $this->post->title,
+        ];
+    }
+}
