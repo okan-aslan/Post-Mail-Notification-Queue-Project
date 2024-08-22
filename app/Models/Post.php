5146d29eaba2b3e04a961d6ceb06fda56ea8a9e8
Okan Aslan <aaslan.okan@gmail.com>
2024-08-22 12:16:07 +0300

    added scopes to query published & pending posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,7 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content'];
+    protected $fillable = ['title', 'content', 'status'];
 
     /**
      * Get the user that owns the Post
@@ -20,5 +21,15 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopePublishedPosts(Builder $query): void
+    {
+        $query->where('status', 'published');
+    }
+
+    public function scopePendingPosts(Builder $query): void
+    {
+        $query->where('status', 'pending');
     }
 }
